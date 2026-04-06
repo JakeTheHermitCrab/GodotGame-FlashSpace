9ac6401484839292956f0459c696596cb1b7a6f3
JakeTheHermitCrab <matthewmartinez1028@gmail.com>
2026-04-05 21:37:42 -0500

    worked on the shotgun
    I want the shotgun to be hit scan because I think it would be more practical during gameplay and it would be simpler to implement.

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,3 +3,5 @@ extends Node
 var isFlashSelected = 1
 var isGunSelected = 0
 var isCompassSelected = 0
+
+var gunShoot = 0
--- scripts/shotgun.gd
+++ scripts/shotgun.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,7 +8,12 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	gunHide()
-
+	if Input.is_action_just_pressed("leftClick"):
+		Global.gunShoot = 1
+		if ray_cast_2d.is_colliding():
+			print("hit")
+		else:
+			print("nothing")
 
 func _on_player_shot_gun_switch() -> void:
 	show()
