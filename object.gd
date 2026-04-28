4ec06ab9f2f88656897f6f9e74fdfc5190e38531
JakeTheHermitCrab <matthewmartinez1028@gmail.com>
2026-04-28 14:28:26 -0500

    something

---
+++ object.gd
@@ -0,0 +1,2 @@
+extends AnimatableBody2D
+# this object needs to randomly spawn outside of the camera and despawn at a certain distance. This object needs to slowly move in a random direction while also spinning
